a966399935ba42141ab7475b60b9e40c26f5a7d0
Cody Lillywhite <codylillyw@gmail.com>
2022-01-26 09:21:00 -0500

    Add firebase signup/logout

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
 import logo from './logo.svg';
 import './App.css';
+import './firebase/config';
+import './pages/Signup';
+import { Route, Switch, BrowserRouter } from 'react-router-dom';
+import Signup from './pages/Signup';
+import { UserProvider} from './firebase/userProvider';
+import Profile from './pages/Profile';
+import Header from './pages/Header';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <UserProvider>
+    <BrowserRouter>
+      <div className="app">
+        <Header></Header>
+        <div className="ui grid container">
+          <Switch>
+            <Route exact path="/signup" component={Signup}/>
+            <Route exact path="/profile/:uid" component={Profile}/>
+          </Switch>
+        </div>
+      </div>
+    </BrowserRouter>
+    </UserProvider>
   );
 }
 
